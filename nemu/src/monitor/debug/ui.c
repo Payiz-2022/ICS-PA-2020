785dd723f21805db2f515f4bf535c6625f59b284
tracer-ics2020 <tracer@njuics.org>
2020-10-02 10:33:34 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 5ef6682d0f504e599510cfbe999b06ca     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  10:33:34 up 24 min,  1 user,  load average: 0.02, 0.03, 0.08

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -127,7 +127,7 @@ static int cmd_x(char *args) {
   printf("%24s:", exp);
   int i = 0;
   while (num--) {
-    printf(" %d", (int)addr[i++]);
+    printf(" %02x", (char)addr[i++]);
   }
   putchar('\n');
   return 0;
