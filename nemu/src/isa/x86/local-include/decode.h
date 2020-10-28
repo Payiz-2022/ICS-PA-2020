6b42dcaa04cbdbc1f96a030604c9a60b20b024d1
tracer-ics2020 <tracer@njuics.org>
2020-10-28 17:55:17 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 301c391038a9436699eb2332f90f973e     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  17:55:17 up  2:22,  1 user,  load average: 0.07, 0.02, 0.00

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -54,7 +54,9 @@ static inline def_DopHelper(SI) {
    *
    operand_imm(s, op, load_val, ???, op->width);
    */
-  TODO();
+  word_t simm = instr_fetch(&s->seq_pc, op->width);
+  operand_imm(s, op, load_val, simm, op->width);
+  
 }
 
 /* I386 manual does not contain this abbreviation.
