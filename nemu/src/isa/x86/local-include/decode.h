a6de33a891bd710c8d1bba0d6a756bc4388858ee
tracer-ics2020 <tracer@njuics.org>
2020-10-28 20:08:01 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 301c391038a9436699eb2332f90f973e     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  20:08:01 up  4:35,  1 user,  load average: 0.16, 0.03, 0.01

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -296,5 +296,5 @@ static inline def_DHelper(out_a2dx) {
 static inline void operand_write(DecodeExecState *s, Operand *op, rtlreg_t* src) {
   if (op->type == OP_TYPE_REG) { rtl_sr(s, op->reg, src, op->width); }
   else if (op->type == OP_TYPE_MEM) { rtl_sm(s, s->isa.mbase, s->isa.moff, src, op->width); }
-  else { assert(0); }
+  // else { assert(0); }
 }
