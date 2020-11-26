a4029b9b8ab71cea7350b63e70b9bb4fc2c9cc36
tracer-ics2020 <tracer@njuics.org>
2020-11-26 13:55:33 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 2eab0609decd49b0a44e2ee641ebceb7     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  13:55:34 up 15:24,  1 user,  load average: 0.14, 0.14, 0.07

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -26,11 +26,13 @@ static inline def_EHelper(cmp) {
   rtl_set_CF(s, s0);
 
   rtl_sub(s, s0, ddest, dsrc1);
+  Log("cmp: dest %x src %x result %x\n", *ddest, *dsrc1, *s0);
   rtl_update_ZFSF(s, s0, id_dest->width);
 
   rtl_xor(s, s1, ddest, dsrc1);
   rtl_xor(s, s2, ddest, s0);
   rtl_and(s, s0, s1, s2);
+  Log("%x AND %x result: %x\n", *s1, *s2, *s0);
   rtl_msb(s, s1, s0, id_dest->width);
   rtl_set_OF(s, s1);
 
