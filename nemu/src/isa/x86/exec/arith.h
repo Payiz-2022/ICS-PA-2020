430e4610f43d869ca3bd626a29c3f51e79af3a5e
tracer-ics2020 <tracer@njuics.org>
2020-11-26 21:23:17 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 2c509893743d4324b8374d19db0e9a36     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  21:23:17 up 13 min,  1 user,  load average: 0.04, 0.04, 0.05

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -25,8 +25,8 @@ static inline void cmp_internal(DecodeExecState *s) {
  
 
 static inline def_EHelper(sub) {
-  rtl_sub(s, s0, ddest, dsrc1);
-  operand_write(s, id_dest, s0);
+  rtl_sub(s, ddest, ddest, dsrc1);
+  // operand_write(s, id_dest, s0);
 
   print_asm_template2(sub);
 }
