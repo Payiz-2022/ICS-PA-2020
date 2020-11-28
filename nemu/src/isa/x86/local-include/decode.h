642bc389dbfdd1f1f7cb41b3e584118c49474f69
tracer-ics2020 <tracer@njuics.org>
2020-11-28 17:14:04 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 2c509893743d4324b8374d19db0e9a36     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  17:14:04 up  8:21,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -196,6 +196,7 @@ static inline def_DHelper(gp7_E) {
 /* used by test in group3 */
 static inline def_DHelper(test_I) {
   decode_op_I(s, id_src1, true);
+  Log("test width: %d\n", id_src1->width);
 }
 
 static inline def_DHelper(SI2E) {
