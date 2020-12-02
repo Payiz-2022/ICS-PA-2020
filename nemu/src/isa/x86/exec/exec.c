671ad811d611d7023e4b1193001de8a4c03d08a5
tracer-ics2020 <tracer@njuics.org>
2020-12-02 15:16:23 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 2c509893743d4324b8374d19db0e9a36     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  15:16:23 up 1 day,  2:40,  1 user,  load average: 0.00, 0.03, 0.00

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -139,12 +139,12 @@ again:
     IDEXW(0x24, I2a, and, 1)
     IDEX (0x25, I2a, and)
 
-    IDEXW(0x26, G2E, sub, 1)
-    IDEX (0x27, G2E, sub)
-    IDEXW(0x28, E2G, sub, 1)
-    IDEX (0x29, E2G, sub)
-    IDEXW(0x2a, I2a, sub, 1)
-    IDEX (0x2b, I2a, sub)
+    IDEXW(0x28, G2E, sub, 1)
+    IDEX (0x29, G2E, sub)
+    IDEXW(0x2a, E2G, sub, 1)
+    IDEX (0x2b, E2G, sub)
+    IDEXW(0x2c, I2a, sub, 1)
+    IDEX (0x2d, I2a, sub)
 
     IDEXW(0x30, G2E, xor, 1)
     IDEX (0x31, G2E, xor)
