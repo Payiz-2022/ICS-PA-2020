0a1714cd4a3d07add0b3a3ebb675246487616040
tracer-ics2020 <tracer@njuics.org>
2020-12-02 18:53:33 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 2c509893743d4324b8374d19db0e9a36     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  18:53:33 up 1 day,  5:35,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,13 +13,69 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   return 0;
 }
 
+void sprint_basic_format(char** pout, char** pin, va_list* args) {
+  switch (**pin) {
+    case 's':
+      ; char *p = va_arg(*args, char*);
+      while (*p) *(*pout++) = *p;
+      break;
+
+    case 'd':
+      ; int val = va_arg(*args, int);
+      int f = 1;
+      if (val < 0) {
+        *(*pout++) = '-';
+        f = -1;
+      }
+      int buf[24] = {0};
+      int i = 0;
+      for (; i < 10 && val; i++) {
+        buf[i] = (val % 10) * f;
+        val /= 10;
+      }
+      for (; i >= 0; i--) {
+        *(*pout++) = buf[i] - '0';
+      }
+      break;
+
+    default:
+      assert(false);
+  }
+  (*pin)++;
+}
+
+void sprint_format(char** pout, char** pin, va_list* args) {
+  switch (**pin) {
+    // To be implemented
+    case '%':
+      *(*pout++) = '%';
+      break;
+
+    case 'l':
+      (*pin)++;
+      sprint_format(pout, pin, args);
+
+    default:
+      sprint_basic_format(pout, pin, args);
+  }
+}
+
 int sprintf(char *out, const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+
   char *pout = out;
   char *pin = (void*)fmt;
   while (*pin) {
-    *pout = *pin;
-    pin++;
-    pout++;
+    switch (*pin) {
+      case '%':
+        pin++;
+        sprint_format(&pout, &pin, &args);
+      default:
+        *pout = *pin;
+        pin++;
+        pout++;
+    }
   }
   return pout - out;
 }
