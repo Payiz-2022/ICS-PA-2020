2d2f30d81ace6807f8cc15f8f882489cafee08c9
tracer-ics2020 <tracer@njuics.org>
2020-12-02 15:45:01 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 2c509893743d4324b8374d19db0e9a36     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-54-generic       Architecture: x86-64  15:45:01 up 1 day,  3:08,  1 user,  load average: 1.49, 0.72, 0.27

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -28,9 +28,9 @@ static inline def_EHelper(sub) {
   rtl_sub(s, s0, ddest, dsrc1);
 
   rtl_update_ZFSF(s, s0, id_dest->width);
-  rtl_is_add_carry(s, s1, s0, dsrc1);
+  rtl_is_sub_carry(s, s1, s0, dsrc1);
   rtl_set_CF(s, s1);
-  rtl_is_add_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
+  rtl_is_sub_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
   rtl_set_OF(s, s1);
   operand_write(s, id_dest, s0);
 
