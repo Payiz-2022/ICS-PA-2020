ebf424df02ea9ebdf4538910a42064584eb20349
tracer-ics2020 <tracer@njuics.org>
2020-12-16 11:53:36 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 473f17caa3474690ae67166933f2cdce     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  11:53:36 up 19 min,  1 user,  load average: 0.18, 0.12, 0.18

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -65,8 +65,8 @@ int sprint_read_pad(char** pout, char** pin, va_list* args) {
   int sum = **pin - '0';
   if (sum < '0' || sum > '9') return 0;
   (*pin)++;
-  int ans = sprint_read_pad(pout, pin, args);
-  if (ans) return sum * 10 + ans;
+  // int ans = sprint_read_pad(pout, pin, args);
+  // if (ans) return sum * 10 + ans;
   return sum;
 }
 
