8d0df7c3d789e88028c3137a4bbdd115e84a637c
tracer-ics2020 <tracer@njuics.org>
2020-12-16 22:31:53 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 473f17caa3474690ae67166933f2cdce     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  22:31:53 up  7:35,  1 user,  load average: 0.08, 0.05, 0.18

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -5,31 +5,50 @@ uint32_t boot_time;
 
 void __am_timer_init() {
   boot_time = inl(RTC_ADDR);
-  // putch('\n');
-  // putch('B');
-  // putch('O');
-  // putch('O');
-  // putch('T');
-  // putch('T');
-  // putch('I');
-  // putch('M');
-  // putch('E');
-  // putch(' ');
-  // unsigned int t = boot_time;
-  // int buf[20] = {0}, i = 0;
-  // while (t) {
-  //   buf[++i] = t % 10;
-  //   t /= 10;
-  // }
-  // if (i == 0) i++;
-  // while (i--) {
-  //   putch('0' + buf[i]);
-  // }
-  // putch('\n');
+  putch('\n');
+  putch('B');
+  putch('O');
+  putch('O');
+  putch('T');
+  putch('T');
+  putch('I');
+  putch('M');
+  putch('E');
+  putch(' ');
+  unsigned int t = boot_time;
+  int buf[20] = {0}, i = 0;
+  while (t) {
+    buf[++i] = t % 10;
+    t /= 10;
+  }
+  if (i == 0) i++;
+  while (i--) {
+    putch('0' + buf[i]);
+  }
+  putch('\n');
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = inl(RTC_ADDR) - boot_time;
+  putch('\n');
+  putch('U');
+  putch('P');
+  putch('T');
+  putch('I');
+  putch('M');
+  putch('E');
+  putch(' ');
+  unsigned int t = boot_time;
+  int buf[20] = {0}, i = 0;
+  while (t) {
+    buf[++i] = t % 10;
+    t /= 10;
+  }
+  if (i == 0) i++;
+  while (i--) {
+    putch('0' + buf[i]);
+  }
+  putch('\n');
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
