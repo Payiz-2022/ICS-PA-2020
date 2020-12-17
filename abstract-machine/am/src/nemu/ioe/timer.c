fd7e8788d3dc2bf0efbf98e87541ffc5cef86966
tracer-ics2020 <tracer@njuics.org>
2020-12-17 23:08:45 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 473f17caa3474690ae67166933f2cdce     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  23:08:45 up 18:22,  1 user,  load average: 0.80, 0.61, 0.52

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -5,27 +5,6 @@ uint64_t boot_time;
 
 void __am_timer_init() {
   boot_time = (uint64_t)inl(RTC_ADDR + 4) * 1000000LL + inl(RTC_ADDR);
-  // putch('\n');
-  // putch('B');
-  // putch('O');
-  // putch('O');
-  // putch('T');
-  // putch('T');
-  // putch('I');
-  // putch('M');
-  // putch('E');
-  // putch(' ');
-  // uint64_t t = boot_time;
-  // int buf[20] = {0}, i = 0;
-  // while (t) {
-  //   buf[++i] = t % 10;
-  //   t /= 10;
-  // }
-  // if (i == 0) i++;
-  // while (i--) {
-  //   putch('0' + buf[i--]);
-  // }
-  // putch('\n');
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
