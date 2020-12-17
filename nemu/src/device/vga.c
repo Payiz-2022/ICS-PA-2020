9ab28ece2395d4e7f2647389409329ec64aec3f2
tracer-ics2020 <tracer@njuics.org>
2020-12-18 00:36:41 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 473f17caa3474690ae67166933f2cdce     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  00:36:41 up 19:50,  1 user,  load average: 0.15, 0.05, 0.07

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -38,6 +38,10 @@ static inline void update_screen() {
 }
 
 void vga_update_screen() {
+  if (*(int *)(vgactl_port_base + 4)) {
+    update_screen();
+    *(int *)(vgactl_port_base + 4) = 0;
+  }
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
 }
