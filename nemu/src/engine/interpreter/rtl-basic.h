363f9f2244798ca84bc845ab9ed0b6252b865c65
tracer-ics2020 <tracer@njuics.org>
2020-12-18 14:30:13 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 473f17caa3474690ae67166933f2cdce     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  14:30:13 up 22:13,  1 user,  load average: 0.09, 0.02, 0.01

--- nemu/src/engine/interpreter/rtl-basic.h
+++ nemu/src/engine/interpreter/rtl-basic.h
@@ -91,7 +91,7 @@ static inline def_rtl(lm, rtlreg_t *dest, const rtlreg_t* addr, word_t offset, i
 static inline def_rtl(sm, const rtlreg_t* addr, word_t offset, const rtlreg_t* src1, int len) {
   vaddr_write(*addr + offset, *src1, len);
   if (*src1 == 0x97979797) {
-    printf("Write into 0x%x pc 0x%x", *addr, cpu.pc);
+    printf("Write into 0x%x pc 0x%x\n", *addr, cpu.pc);
   }
 }
 
