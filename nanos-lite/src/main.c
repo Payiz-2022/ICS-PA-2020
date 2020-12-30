7dfbb84b5f374e04c6590088d97f42bfcd492ae5
tracer-ics2020 <tracer@njuics.org>
2020-12-31 02:01:35 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 40fd3a88b1ab4ea185613a6befe014b4     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  02:01:35 up  6:01,  1 user,  load average: 0.81, 0.27, 0.18

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -8,8 +8,8 @@ void init_fs(void);
 void init_proc(void);
 
 int main() {
-  return 0;
   extern const char logo[];
+  return 0;
   printf("%s", logo);
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
