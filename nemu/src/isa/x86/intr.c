4c7c494ca0bafde880237ca3870fb9fb6dbe9dce
tracer-ics2020 <tracer@njuics.org>
2020-12-31 02:43:36 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 40fd3a88b1ab4ea185613a6befe014b4     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  02:43:36 up  6:43,  1 user,  load average: 0.07, 0.12, 0.09

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -5,13 +5,14 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  assert(0);
+
   vaddr_t intr_gate = cpu.idtr.base + NO * 64;
   uint32_t gate_l = vaddr_read(intr_gate, 2);
   uint32_t gate_h = vaddr_read(intr_gate + 4, 4);
   if ((gate_h & 0x8000) == 0) return;
 
   vaddr_t intr_addr = ((gate_h >> 16) << 16) | gate_l;
+  printf("intr_addr: 0x%x\n", intr_addr);
 
   rtl_li(s, s0, cpu.eflags.val);
   rtl_push(s, s0);
