1e95af5efbf266701a14ddb143d2be767ca348d2
tracer-ics2020 <tracer@njuics.org>
2020-12-31 11:47:49 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 40fd3a88b1ab4ea185613a6befe014b4     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  11:47:49 up  9:13,  1 user,  load average: 0.25, 0.13, 0.05

--- abstract-machine/am/include/arch/x86-nemu.h
+++ abstract-machine/am/include/arch/x86-nemu.h
@@ -2,12 +2,18 @@
 #define ARCH_H__
 
 struct Context {
-  uintptr_t e;
-  uintptr_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-  int irq;
+  uintptr_t eflags, cs, eip;
   void *cr3;
-  uintptr_t eip, cs, eflags;
+  int irq;
+  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
 };
+
+// struct Context {
+//   uintptr_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+//   int irq;
+//   void *cr3;
+//   uintptr_t eip, cs, eflags;
+// };
 
 // struct Context {
 //   uintptr_t esi, ebx, eax, eip, edx, eflags, ecx, cs, esp, edi, ebp;
