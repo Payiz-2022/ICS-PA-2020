02e2d64e10ee7ecd05df19b63df14c4137df678d
tracer-ics2020 <tracer@njuics.org>
2020-12-31 11:11:46 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 40fd3a88b1ab4ea185613a6befe014b4     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  11:11:46 up  8:37,  1 user,  load average: 0.10, 0.13, 0.07

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -191,7 +191,7 @@ static inline def_DHelper(setcc_E) {
 
 static inline def_DHelper(gp7_E) {
   operand_rm(s, id_dest, false, NULL, false);
-  printf("dest val: %p\n", id_dest);
+  printf("dest val: 0x%x mbase: 0x%x\n", id_dest->val, *s->isa.mbase);
 }
 
 /* used by test in group3 */
