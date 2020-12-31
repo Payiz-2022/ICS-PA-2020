e284f483e09687b3830802b74df09c793dafb873
191220052-Li Songyuan <illimosity@gmail.com>
2020-12-31 14:53:31 +0800

    navy-apps ics2020 initialized
    navy-apps 9e33d06ecfc9505261f28987b5e78bb116011e57 apps,busybox: add missing files

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -24,7 +24,6 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
 void query_intr(DecodeExecState *s) {
   rtl_pop(s, s0);
   rtl_jr(s, s0);
-  printf("Jump back to 0x%x\n", *s0);
 
   rtl_pop(s, s0);
   cpu.cs = *s0;
