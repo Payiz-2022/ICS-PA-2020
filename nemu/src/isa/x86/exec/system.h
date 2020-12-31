5d39829db216bb43be55ba98a959eef058b1ce91
tracer-ics2020 <tracer@njuics.org>
2020-12-31 11:13:03 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 40fd3a88b1ab4ea185613a6befe014b4     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  11:13:03 up  8:38,  1 user,  load average: 0.11, 0.12, 0.07

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -2,8 +2,8 @@
 void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr);
 
 static inline def_EHelper(lidt) {
-  cpu.idtr.limit = vaddr_read(*ddest, 2);
-  cpu.idtr.base = vaddr_read(*ddest + 2, 4);
+  cpu.idtr.limit = vaddr_read(*s->isa.mbase, 2);
+  cpu.idtr.base = vaddr_read(*s->isa.mbase + 2, 4);
   printf("lidt addr: 0x%x, limit: 0x%x, base: 0x%x\n", *ddest, cpu.idtr.limit, cpu.idtr.base);
 
   print_asm_template1(lidt);
