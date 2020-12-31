f1add3e56d2d9fc31e6649a18f7b120804f2b052
tracer-ics2020 <tracer@njuics.org>
2020-12-31 14:05:54 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 40fd3a88b1ab4ea185613a6befe014b4     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  14:05:54 up 10:27,  1 user,  load average: 0.24, 0.09, 0.02

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -11,8 +11,10 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
   vaddr_t intr_addr = ((gate_h >> 16) << 16) | gate_l;
 
   rtl_push(s, &cpu.eflags.val);
+
   rtl_li(s, s0, cpu.cs);
   rtl_push(s, s0);
+
   rtl_push(s, &cpu.pc);
 
   rtl_j(s, intr_addr);
@@ -21,8 +23,10 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
 void query_intr(DecodeExecState *s) {
   rtl_pop(s, s0);
   rtl_jr(s, s0);
+  printf("Jump back to 0x%x\n", *s0);
+
   rtl_pop(s, s0);
   cpu.cs = *s0;
+
   rtl_pop(s, &cpu.eflags.val);
-  printf("Jump back to 0x%x\n", *s0);
 }
