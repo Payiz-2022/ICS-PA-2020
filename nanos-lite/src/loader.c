c9543c2def9ac22a39931abe7be309ef02b2eb41
tracer-ics2020 <tracer@njuics.org>
2021-01-05 16:37:09 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 40fd3a88b1ab4ea185613a6befe014b4     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  16:37:09 up 18:09,  1 user,  load average: 0.01, 0.01, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,11 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+#define PMEM_START 0x3000000
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  ramdisk_read((void*)0x3000000, 0, get_ramdisk_size());
-  return 0x300122a;
+  ramdisk_read((void*)PMEM_START, 0, get_ramdisk_size());
+  return *(int*)(PMEM_START + 0x18);
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
