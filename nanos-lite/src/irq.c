014d8da442095699bf6d569df1578714ac14cfe8
tracer-ics2020 <tracer@njuics.org>
2021-01-08 01:41:24 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  01:41:24 up 21 min,  1 user,  load average: 0.32, 0.07, 0.06

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,8 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: printf("自陷干啥？别想不开啊！\n"); break;
+    case EVENT_SYSCALL: printf("Received a syscall.\n"); break;
+    case EVENT_YIELD: printf("Received a yield event.\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
