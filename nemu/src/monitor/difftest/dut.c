54ebaf7fb6b73d6275f9cc6eb705f08f059537f5
tracer-ics2020 <tracer@njuics.org>
2021-01-08 02:23:00 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  02:23:00 up  1:02,  1 user,  load average: 0.07, 0.07, 0.07

--- nemu/src/monitor/difftest/dut.c
+++ nemu/src/monitor/difftest/dut.c
@@ -78,8 +78,10 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
+    printf("== CURRENT ENV ==\n");
     isa_reg_display();
     memcpy((void*)&cpu, (void*)ref, 8 * sizeof(rtlreg_t));
+    printf("== REFERENCE ENV ==\n");
     isa_reg_display();
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
