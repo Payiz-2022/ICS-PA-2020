d52b1f024be0b5c443fea043f05e412cfa29e3ef
tracer-ics2020 <tracer@njuics.org>
2021-01-08 19:23:08 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  19:23:09 up  7:45,  1 user,  load average: 0.08, 0.04, 0.07

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,6 +3,10 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+  halt(0);
 
   switch (a[0]) {
     case SYS_exit: halt(0); break;
