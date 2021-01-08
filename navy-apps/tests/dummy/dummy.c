8fb1846f86837467ceec4455059c5d21958371f3
tracer-ics2020 <tracer@njuics.org>
2021-01-08 19:45:18 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  19:45:18 up  8:07,  1 user,  load average: 0.05, 0.06, 0.08

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,13 +1,13 @@
-// #include <stdint.h>
+#include <stdint.h>
 
-// #ifdef __ISA_NATIVE__
-// #error can not support ISA=native
-// #endif
+#ifdef __ISA_NATIVE__
+#error can not support ISA=native
+#endif
 
-// #define SYS_yield 1
-// extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
+#define SYS_yield 1
+extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  // _syscall_(SYS_yield, 0, 0, 0);
+  _syscall_(SYS_yield, 0, 0, 0);
   return 0;
 }
