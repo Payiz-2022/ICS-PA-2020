90fa24dbbc7aff4c7629d7460100bafee0f30a05
tracer-ics2020 <tracer@njuics.org>
2021-01-08 18:59:04 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  18:59:04 up  7:21,  1 user,  load average: 0.05, 0.01, 0.00

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -5,8 +5,9 @@
 #endif
 
 #define SYS_yield 1
-extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
+// extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  return _syscall_(SYS_yield, 0, 0, 0);
+  // _syscall_(SYS_yield, 0, 0, 0);
+  return 0;
 }
