761fc6f492c47c92d67d4139ae3caaed9a7ef4b9
tracer-ics2020 <tracer@njuics.org>
2021-01-08 11:28:53 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  11:28:53 up  1:53,  1 user,  load average: 0.28, 0.13, 0.04

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -25,8 +25,8 @@ static inline def_rtl(sr, int r, const rtlreg_t* src1, int width) {
 }
 
 static inline def_rtl(push, const rtlreg_t* src1) {
-  rtl_sm(s, &cpu.esp, 0, src1, 4);
   rtl_subi(s, &cpu.esp, &cpu.esp, 4);
+  rtl_sm(s, &cpu.esp, 0, src1, 4);
 }
 
 static inline def_rtl(pop, rtlreg_t* dest) {
