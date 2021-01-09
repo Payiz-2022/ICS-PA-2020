0df82db89a57d092fa2d223a65fe70d953d98119
tracer-ics2020 <tracer@njuics.org>
2021-01-10 00:44:18 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  00:44:18 up 16:43,  1 user,  load average: 0.00, 0.06, 0.06

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -4,7 +4,7 @@
 #include "difftest.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  if (memcmp((void*)ref_r->gpr, (void*)cpu.gpr, 8 * sizeof(rtlreg_t)) == 0 
+  if (memcmp((void*)(ref_r->gpr + 1), (void*)(cpu.gpr + 1), 2 * sizeof(rtlreg_t)) == 0 
       && ref_r->eip == cpu.eip)
     return true;
   return false;
