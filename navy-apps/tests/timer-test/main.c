c18f4b7cecc032d2f8ca100ec837cb94be482d46
tracer-ics2020 <tracer@njuics.org>
2021-01-14 13:22:30 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  13:22:30 up 1 day, 12:03,  1 user,  load average: 0.55, 0.29, 0.12

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -5,7 +5,8 @@ int main() {
   struct timeval current_time;
   gettimeofday(&current_time, NULL);
   while (1) {
-    // if (current_time.tv_usec % 1000 == 0)
+    gettimeofday(&current_time, NULL);
+    if (current_time.tv_usec % 1000 == 0)
       printf("usec: %ld\n", current_time.tv_usec);
   }
   return 0;
