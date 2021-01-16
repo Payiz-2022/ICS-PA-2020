ff4e3b7b2f30b248c82b607ed621843e63862f46
tracer-ics2020 <tracer@njuics.org>
2021-01-16 16:48:53 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  16:48:53 up 1 day, 13:24,  1 user,  load average: 0.12, 0.12, 0.10

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,7 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  putch('f');
+  if (len) putch('f');
   char* p = (char*)buf;
   for (int i = 0; i < len; i++) {
     putch(*p);
