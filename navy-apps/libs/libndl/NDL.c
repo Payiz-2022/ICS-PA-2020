a6b4bfa8de2e2726f8c7b4c66831de712a8f5c2a
tracer-ics2020 <tracer@njuics.org>
2021-01-16 15:47:37 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  15:47:37 up 1 day, 12:22,  1 user,  load average: 0.02, 0.47, 0.53

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -9,7 +9,9 @@ static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
 uint32_t NDL_GetTicks() {
-  return 0;
+  struct timeval current_time;
+  gettimeofday(&current_time, NULL);
+  return current_time.tv_sec / 1000;
 }
 
 int NDL_PollEvent(char *buf, int len) {
--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,14 +2,22 @@
 #include <stdio.h>
 
 int main() {
-  struct timeval current_time;
-  gettimeofday(&current_time, NULL);
-  long int last_sec = -1;
+  // struct timeval current_time;
+  // gettimeofday(&current_time, NULL);
+  // long int last_sec = -1;
+  // while (1) {
+  //   gettimeofday(&current_time, NULL);
+  //   if (current_time.tv_sec != last_sec) {
+  //     last_sec = current_time.tv_sec;
+  //     printf("Second: %ld Usec: %ld\n", current_time.tv_sec, current_time.tv_usec);
+  //   }
+  // }
+  uint32_t current_msec, last_sec = -1;
   while (1) {
-    gettimeofday(&current_time, NULL);
-    if (current_time.tv_sec != last_sec) {
-      last_sec = current_time.tv_sec;
-      printf("Second: %ld Usec: %ld\n", current_time.tv_sec, current_time.tv_usec);
+    current_msec = NDL_GetTicks();
+    if (current_msec / 1000 != last_sec) {
+      last_sec = current_msec / 1000;
+      printf("MilliSecond: %d\n", last_sec);
     }
   }
   return 0;
