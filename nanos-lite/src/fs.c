205585bf264cb44242d0514593a775f012c992a1
tracer-ics2020 <tracer@njuics.org>
2021-01-18 06:19:34 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  06:19:34 up 1 day, 19:04,  1 user,  load average: 0.69, 0.48, 0.31

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -36,7 +36,7 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
   [FD_FB] = {"/dev/fb", 0, 0, invalid_read, fb_write},
   {"/dev/events", 0, 0, events_read, invalid_write},
-  {"/proc/dispinfo", 64, 0, dispinfo_read, invalid_write},
+  {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
