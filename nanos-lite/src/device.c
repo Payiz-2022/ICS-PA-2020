d5942a87aef42221184293ce62c587b3728735e9
tracer-ics2020 <tracer@njuics.org>
2021-01-17 15:48:24 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  15:48:24 up 1 day, 15:13,  1 user,  load average: 0.00, 0.02, 0.00

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -26,9 +26,9 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE) return 0;
-  printf("len: %d\n", len);
-  int ret = snprintf(buf, len, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode]);
-  printf("printed: %d\n", ret);
+  // int ret = snprintf(buf, len, "%s %s", ev.keydown ? "kd" : "ku", keyname[ev.keycode]);
+  int ret = snprintf(buf, len, "sdfa");
+  printf("count: %d\n", ret);
   return ret;
 }
 
