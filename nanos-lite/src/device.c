a838ed7e4dd1acdfb9f6810d1456adf3226b41fd
tracer-ics2020 <tracer@njuics.org>
2021-01-18 08:58:42 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  08:58:42 up 1 day, 21:43,  1 user,  load average: 0.82, 0.49, 0.25

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -37,6 +37,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+  printf("fb_write: offset %d, len %d\n", offset, len);
   AM_GPU_CONFIG_T cfg;
   ioe_read(AM_GPU_CONFIG, &cfg);
   uint32_t start_x = offset / sizeof(uint32_t) % cfg.width;
