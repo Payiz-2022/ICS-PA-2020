5605d7372265158e74fd46cc659737ba070973cf
tracer-ics2020 <tracer@njuics.org>
2021-01-20 01:02:29 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  01:02:29 up 2 days,  9:00,  1 user,  load average: 0.55, 0.20, 0.19

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -4,6 +4,7 @@
 
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
+#include <stdio.h>
 
 SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(src->type == RW_TYPE_MEM);
@@ -11,7 +12,19 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  FILE *img = fopen(filename, "r");
+  assert(img);
+  fseek(img, 0, SEEK_END);
+  size_t size = ftell(img);
+  fseek(img, 0, SEEK_SET);
+
+  unsigned char* buf = malloc(size);
+  fread(buf, size, 1, img);
+  SDL_Surface* ret = STBIMG_LoadFromMemory(buf, size);
+  fclose(img);
+  free(buf);
+
+  return ret;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
