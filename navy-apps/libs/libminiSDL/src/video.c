fbd50bb5afaf13c5a5bbd7e5ed4354f837dd6dc3
tracer-ics2020 <tracer@njuics.org>
2021-01-19 17:49:16 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  17:49:17 up 2 days,  3:59,  1 user,  load average: 0.08, 0.02, 0.01

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -8,24 +8,27 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
 
-  int x = dstrect->x, y = dstrect->y, w = dstrect->w, h = dstrect->h;
-  for (int i = y; i < y + h; i++)
-    for (int j = x; j < x + w; j++)
-      dst->pixels[i * dst->w + j] = src->pixels[i * src->w + j];
+  // int x = dstrect->x, y = dstrect->y, w = dstrect->w, h = dstrect->h;
+  // for (int i = y; i < y + h; i++)
+  //   for (int j = x; j < x + w; j++)
+  //     dst->pixels[i * dst->w + j] = src->pixels[i * src->w + j];
 
-  SDL_UpdateRect(dst, x, y, w, h);
+  // SDL_UpdateRect(dst, x, y, w, h);
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
-  int x = dstrect->x, y = dstrect->y, w = dstrect->w, h = dstrect->h;
-  for (int i = y; i < y + h; i++)
-    for (int j = x; j < x + w; j++)
-      dst->pixels[i * dst->w + j] = color;
-  SDL_UpdateRect(dst, x, y, w, h);
+  // int x = dstrect->x, y = dstrect->y, w = dstrect->w, h = dstrect->h;
+  // for (int i = y; i < y + h; i++)
+  //   for (int j = x; j < x + w; j++)
+  //     dst->pixels[i * dst->w + j] = color;
+  // SDL_UpdateRect(dst, x, y, w, h);
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
-  NDL_DrawRect((uint32_t*)(s->pixels), x, y, w, h);
+  if (x == 0 && y == 0 && w == 0 && h == 0)
+    NDL_DrawRect(s->pixels, 0, 0, s->width, s->height);
+  else
+    NDL_DrawRect(s->pixels, x, y, w, h);
 }
 
 // APIs below are already implemented.
