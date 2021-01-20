914643d2aa8ab1c7538318f7b4672ee961eb9a19
tracer-ics2020 <tracer@njuics.org>
2021-01-20 21:24:59 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  21:24:59 up 2 days, 19:40,  1 user,  load average: 1.08, 0.67, 0.43

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -82,7 +82,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   }
   for (int i = 0; i < h; i++)
     for (int j = 0; j < w; j++)
-      canvas[(y + i) * canvas_w + (x + j)] = pixels[i * w + j];
+      canvas[(y + i) * canvas_w + (x + j)] = pixels[i * canvas_w + j];
 
   NDL_UpdateCanvas();
 }
