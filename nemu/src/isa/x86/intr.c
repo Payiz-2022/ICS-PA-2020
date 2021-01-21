2d1a65a5473ae42e4d390a82e9782195a0c3000d
tracer-ics2020 <tracer@njuics.org>
2021-01-21 20:57:28 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  20:57:28 up 3 days,  8:48,  1 user,  load average: 0.11, 0.05, 0.01

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -13,6 +13,7 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
   rtl_push(s, &cpu.eflags.val);
 
   rtl_li(s, s0, cpu.cs);
+  printf("[raise_intr] cs: 0x%08x\n", cpu.cs);
   rtl_push(s, s0);
 
   rtl_li(s, s0, ret_addr);
@@ -28,6 +29,7 @@ void query_intr(DecodeExecState *s) {
   rtl_jr(s, s0);
 
   rtl_pop(s, s0);
+  printf("[iret] cs: 0x%08x\n", *s0);
   cpu.cs = *s0;
 
   rtl_pop(s, &cpu.eflags.val);
