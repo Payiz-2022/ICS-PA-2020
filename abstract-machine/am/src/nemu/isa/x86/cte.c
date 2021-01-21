bb8bbdcafa99533a223f7be64e04bef77ab70976
tracer-ics2020 <tracer@njuics.org>
2021-01-21 19:26:45 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  19:26:45 up 3 days,  7:17,  1 user,  load average: 0.16, 0.10, 0.04

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -56,7 +56,7 @@ Context* kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *c = (Context*)kstack.end - 30;
   c->eip = (uintptr_t)entry;
   *((uintptr_t*)(c + 1) + 1) = (uintptr_t)arg;
-  // printf("Stack: 0x%08x - 0x%08x  C: 0x%08x\n", kstack.start, kstack.end, c);
+  printf("Stack: 0x%08x - 0x%08x  C: 0x%08x\n", kstack.start, kstack.end, c);
   return c;
 }
 
