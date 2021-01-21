f6b98e31799d9389b41a99b8d7a9ec7556331bf8
tracer-ics2020 <tracer@njuics.org>
2021-01-21 18:55:26 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  18:55:26 up 3 days,  6:46,  1 user,  load average: 0.01, 0.01, 0.00

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -26,7 +26,7 @@ Context* __am_irq_handle(Context *c) {
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-
+  printf("irq_handle C: 0x%08x\n", c);
   return c;
 }
 
@@ -56,7 +56,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context* kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *c = (Context*)kstack.end - 1;
   c->eip = (uintptr_t)entry;
-  // *((uintptr_t*)kstack.end + 1) = (uintptr_t)arg;
+  *((uintptr_t*)kstack.end + 1) = (uintptr_t)arg;
   printf("Stack: 0x%08x - 0x%08x  C: 0x%08x\n", kstack.start, kstack.end, c);
   return c;
 }
