06d7d6b0a988a2ada8d0a3d7990631fa85f5cacc
tracer-ics2020 <tracer@njuics.org>
2021-01-22 00:23:06 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  00:23:06 up 3 days, 12:13,  1 user,  load average: 0.44, 0.16, 0.05

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,13 +28,13 @@ void init_proc() {
   // naive_uload(NULL, "/bin/menu");
 
   context_kload(&pcb[0], hello_fun, (void*)0xeeeee);
-  context_uload(&pcb[1], "/bin/pal");
+  context_uload(&pcb[3], "/bin/pal");
   switch_boot_pcb();
 }
 
 Context* schedule(Context *prev) {
   current->cp = prev;
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  current = (current == &pcb[0] ? &pcb[3] : &pcb[0]);
   printf("current cp: 0x%08x\n", current->cp);
   return current->cp;
 }
