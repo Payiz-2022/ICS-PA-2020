de23a5af8e02ad6572d51c47e20090e11e36894d
tracer-ics2020 <tracer@njuics.org>
2021-01-22 12:41:34 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  12:41:34 up 3 days, 16:43,  1 user,  load average: 0.12, 0.24, 0.16

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  printf("Running with argc %d\n", argc);
+  printf("Running with argc %d argv %s\n", argc, argc == 2 ? argv[1] : "NULL");
   int n = (argc >= 2 ? atoi(argv[1]) : 1);
   printf("%s: argv[1] = %d\n", argv[0], n);
 
