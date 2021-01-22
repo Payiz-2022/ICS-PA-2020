3d25dd889e21f97afc51c31670cca24492679e4d
tracer-ics2020 <tracer@njuics.org>
2021-01-22 13:04:44 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  13:04:44 up 3 days, 17:06,  1 user,  load average: 0.03, 0.03, 0.03

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -81,6 +81,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     mem_top -= strlen(*p) + 1;
     strcpy(mem_top, *p);
     *(uintptr_t*)argv_start = (uintptr_t)mem_top;
+    printf("+args: %s\n", (char*)argv_start);
     argv_start += sizeof(uintptr_t);
     p++;
   }
