dca9ff09d8a3564d221f64ee26661352c28ae463
tracer-ics2020 <tracer@njuics.org>
2021-01-23 22:36:32 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  22:36:32 up 4 days, 18:29,  1 user,  load average: 0.30, 0.18, 0.12

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -4,12 +4,10 @@
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {
   paddr_t pg_table = paddr_read(cpu.cr3 + (vaddr >> 22) * sizeof(paddr_t), sizeof(paddr_t));
-  if ((pg_table & 1) == 0) {
-    printf("pg_table: 0x%08x, vaddr: 0x%08x, type: %d, len: %d\n", pg_table, vaddr, type, len);
-  }
-  assert(pg_table & 1);
-  pg_table = pg_table & ADDRMASK;
-  paddr_t pg_base = paddr_read(pg_table + ((vaddr >> 12) & 0x3ff) * sizeof(paddr_t), sizeof(paddr_t)) & ADDRMASK;
+  assert(pg_table & 0x1);
+  paddr_t pg_base = paddr_read((pg_table & ADDRMASK) + ((vaddr >> 12) & 0x3ff) * sizeof(paddr_t), sizeof(paddr_t));
+  assert(pg_base & 0x1);
+  pg_base = pg_base & ADDRMASK;
   // if (vaddr >= 0x40000000)
   //   printf("Translating 0x%08x to 0x%08x\n", vaddr, pg_base);
   if ((vaddr & FLAGMASK) + len - 1 > FLAGMASK)
