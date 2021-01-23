60a2671366ffe7a6929df814247276453b8cd5d2
tracer-ics2020 <tracer@njuics.org>
2021-01-23 16:39:42 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  16:39:42 up 4 days, 12:32,  1 user,  load average: 0.23, 0.11, 0.03

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -5,6 +5,7 @@
 paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {
   paddr_t pg_table = paddr_read(cpu.cr3 + (vaddr >> 22) * sizeof(paddr_t), sizeof(paddr_t)) & ADDRMASK;
   paddr_t pg_base = paddr_read(pg_table + ((vaddr >> 12) & 0x3ff) * sizeof(paddr_t), sizeof(paddr_t)) & ADDRMASK;
+  printf("Translating 0x%08x to 0x%08x\n", vaddr, pg_base);
   if ((vaddr & ADDRMASK) != ((vaddr + len) & ADDRMASK))
     return (pg_base & ADDRMASK) | MEM_RET_CROSS_PAGE;
   return (pg_base & ADDRMASK) | MEM_RET_OK;
