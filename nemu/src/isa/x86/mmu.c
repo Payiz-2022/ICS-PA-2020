310dfdf2233fcdeee626c3d2bbf888c4a0665d38
tracer-ics2020 <tracer@njuics.org>
2021-01-23 21:35:27 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  21:35:27 up 4 days, 17:28,  1 user,  load average: 0.24, 0.22, 0.11

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -4,10 +4,11 @@
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {
   paddr_t pg_table = paddr_read(cpu.cr3 + (vaddr >> 22) * sizeof(paddr_t), sizeof(paddr_t)) & ADDRMASK;
-  paddr_t pg_base = paddr_read(pg_table + ((vaddr >> 12) & 0x3ff) * sizeof(paddr_t), sizeof(paddr_t)) & ADDRMASK;
+  paddr_t paddr = paddr_read(pg_table + ((vaddr >> 12) & 0x3ff) * sizeof(paddr_t), sizeof(paddr_t));
+  paddr_t pg_base = paddr & ADDRMASK;
   // if (vaddr >= 0x40000000)
   //   printf("Translating 0x%08x to 0x%08x\n", vaddr, pg_base);
-  if ((vaddr & ADDRMASK) != ((vaddr + len - 1) & ADDRMASK))
-    return (pg_base & ADDRMASK) | MEM_RET_CROSS_PAGE;
-  return (pg_base & ADDRMASK) | MEM_RET_OK;
+  if (pg_base != ((paddr + len - 1) & ADDRMASK))
+    return pg_base | MEM_RET_CROSS_PAGE;
+  return pg_base | MEM_RET_OK;
 }
