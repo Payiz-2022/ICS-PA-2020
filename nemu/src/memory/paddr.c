5d364e277424b80cef8178b8a70b356146085dfb
tracer-ics2020 <tracer@njuics.org>
2021-01-23 13:10:52 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  13:10:52 up 4 days,  9:03,  1 user,  load average: 0.61, 0.34, 0.14

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -62,7 +62,7 @@ inline void paddr_write(paddr_t addr, word_t data, int len) {
 word_t vaddr_mmu_read(vaddr_t addr, int len, int type) {
   paddr_t pg_base = isa_mmu_translate(addr, type, len);
   if ((pg_base & MEM_RET_OK) == MEM_RET_OK) {
-    paddr_t paddr = pg_base | (addr & 0xfff);
+    paddr_t paddr = (pg_base & 0xfffff000) | (addr & 0xfff);
     return paddr_read(paddr, len);
   }
   assert(false);
@@ -72,7 +72,7 @@ word_t vaddr_mmu_read(vaddr_t addr, int len, int type) {
 void vaddr_mmu_write(vaddr_t addr, word_t data, int len) {
   paddr_t pg_base = isa_mmu_translate(addr, MEM_TYPE_WRITE, len);
   if ((pg_base & MEM_RET_OK) == MEM_RET_OK) {
-    paddr_t paddr = pg_base | (addr & 0xfff);
+    paddr_t paddr = (pg_base & 0xfffff000) | (addr & 0xfff);
     paddr_write(paddr, data, len);
     return;
   }
