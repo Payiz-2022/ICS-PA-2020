4a1f70d367b7696ad692fc3a2dcfec8a6b302216
tracer-ics2020 <tracer@njuics.org>
2021-01-23 11:37:47 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  11:37:47 up 4 days,  7:30,  1 user,  load average: 0.27, 0.13, 0.09

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -3,6 +3,7 @@
 #include <memory/paddr.h>
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {
+  assert(0);
   paddr_t pg_table = paddr_read(cpu.cr3 + (vaddr >> 22) * sizeof(paddr_t), sizeof(paddr_t));
   paddr_t pg_base = paddr_read(pg_table + ((vaddr >> 12) & 0x3ff) * sizeof(paddr_t), sizeof(paddr_t));
   printf("vaddr 0x%08x, pg_base 0x%08x\n", vaddr, pg_base);
