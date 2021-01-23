c1ce44519d580a7c3471e99bbc92e41cfd337b43
tracer-ics2020 <tracer@njuics.org>
2021-01-23 11:18:10 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  11:18:10 up 4 days,  7:11,  1 user,  load average: 0.15, 0.05, 0.05

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -11,18 +11,18 @@ static inline def_EHelper(lidt) {
 }
 
 static inline def_EHelper(mov_r2cr) {
-  printf("mov_r2cr: dsrc1 0x%08x, dest 0x%08x\n", *dsrc1, *ddest);
+  printf("mov_r2cr: dsrc1 0x%08x, dest operand reg: %d\n", *dsrc1, id_dest->reg);
   operand_write(s, id_dest, dsrc1);
   printf("cr3: 0x%08lx\n", cpu.cr3);
 
-  printf("movl %%%s,%%cr%d", reg_name(id_src1->reg, 4), id_dest->reg);
+  print_asm("movl %%%s,%%cr%d", reg_name(id_src1->reg, 4), id_dest->reg);
 }
 
 static inline def_EHelper(mov_cr2r) {
   printf("mov_cr2r: dsrc1 0x%08x, dest 0x%08x\n", *dsrc1, *ddest);
   operand_write(s, id_dest, dsrc1);
 
-  printf("movl %%cr%d,%%%s", id_src1->reg, reg_name(id_dest->reg, 4));
+  print_asm("movl %%cr%d,%%%s", id_src1->reg, reg_name(id_dest->reg, 4));
 
 #ifndef __DIFF_REF_NEMU__
   difftest_skip_ref();
