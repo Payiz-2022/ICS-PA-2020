3056ad6a7d8852d3dc616633a5fed31747e25fc6
tracer-ics2020 <tracer@njuics.org>
2021-01-23 13:10:08 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 62f21564b3be4656a49bf37e2c4d281a     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  13:10:08 up 4 days,  9:03,  1 user,  load average: 0.61, 0.31, 0.12

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -9,6 +9,6 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {
   // printf("pg_table 0x%08x pg_base 0x%08x\n", pg_table, pg_base);
   if ((vaddr & 0xfffff000) != ((vaddr + len) & 0xfffff000))
     return (pg_base & 0xfffff000) | MEM_RET_CROSS_PAGE;
-  // return (vaddr & 0xfffff000) | MEM_RET_OK;
+  return (vaddr & 0xfffff000) | MEM_RET_OK;
   return (pg_base & 0xfffff000) | MEM_RET_OK;
 }
