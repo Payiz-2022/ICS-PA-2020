d7fe832f31aa8f890d0e63c999e5f2d0c2484773
tracer-ics2020 <tracer@njuics.org>
2021-01-24 08:51:37 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 71d6739720d24cf492c5d295d069bf32     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-62-generic       Architecture: x86-64  08:51:37 up  1:35,  1 user,  load average: 0.14, 0.06, 0.08

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -65,9 +65,9 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 extern char end;
-intptr_t program_break = (intptr_t)&end;
 char sbrk_buf[40];
 void *_sbrk(intptr_t increment) {
+  intptr_t program_break = (intptr_t)&end;
   sprintf(sbrk_buf, "cur: 0x%08x, inc: 0x%x\n", program_break, increment);
   _write(1, sbrk_buf, 33);
   intptr_t target_brk = program_break + increment;
