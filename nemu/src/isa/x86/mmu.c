7a234c4d5ce88acae0a2e50f7ede52bd11fa23ac
tracer-ics2020 <tracer@njuics.org>
2021-01-24 10:57:18 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 71d6739720d24cf492c5d295d069bf32     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-62-generic       Architecture: x86-64  10:57:18 up  3:41,  1 user,  load average: 0.04, 0.07, 0.16

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -10,7 +10,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {
   assert(pg_table & 0x1);
   paddr_t pg_base = paddr_read((pg_table & ADDRMASK) + ((vaddr >> 12) & 0x3ff) * sizeof(paddr_t), sizeof(paddr_t));
   if ((pg_base & 0x1) == 0) {
-    printf("pbase: 0x%08x, vaddr: 0x%08x, type: %d, len: %d, pc: 0x%08x\n", pg_base, vaddr, type, len, cpu.eip);
+    printf("cr3: 0x%08x, ptable: 0x%08x, pbase: 0x%08x, vaddr: 0x%08x, type: %d, len: %d, pc: 0x%08x\n", cpu.cr3, pg_table, pg_base, vaddr, type, len, cpu.eip);
   }
   assert(pg_base & 0x1);
   pg_base = pg_base & ADDRMASK;
