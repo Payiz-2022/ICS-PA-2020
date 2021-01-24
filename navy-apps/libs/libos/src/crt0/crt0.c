82dc9265e25b42362506e62d3c63374fa92772d4
tracer-ics2020 <tracer@njuics.org>
2021-01-24 11:04:53 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 71d6739720d24cf492c5d295d069bf32     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-62-generic       Architecture: x86-64  11:04:53 up  3:48,  1 user,  load average: 0.53, 0.26, 0.21

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -6,6 +6,7 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
+  assert(0);
   char *empty[] =  {NULL };
   environ = empty;
   exit(main(*args, (char**)(args + 1), empty));
