c0b2c7f424f54e24a8b6a2449405acb4151edd6d
tracer-ics2020 <tracer@njuics.org>
2021-01-24 19:47:43 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 71d6739720d24cf492c5d295d069bf32     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-62-generic       Architecture: x86-64  19:47:43 up 12:31,  1 user,  load average: 0.14, 0.07, 0.07

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,9 +27,9 @@ void init_proc() {
 
   // context_kload(&pcb[0], hello_fun, (void*)0xeeeee);
   char* args[] = {"/bin/pal", "--skip", NULL};
-  context_uload(&pcb[0], "/bin/pal", args, NULL);
+  context_uload(&pcb[1], "/bin/pal", args, NULL);
   // args[0] = "/bin/hello";
-  // context_uload(&pcb[1], "/bin/hello", args, NULL);
+  // context_uload(&pcb[2], "/bin/hello", args, NULL);
   switch_boot_pcb();
 }
 
