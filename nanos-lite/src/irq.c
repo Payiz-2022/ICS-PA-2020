a7f7d546469f1321b03a35f52688121965477d28
tracer-ics2020 <tracer@njuics.org>
2021-01-24 19:39:59 +0800

    >  compile 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 71d6739720d24cf492c5d295d069bf32     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-62-generic       Architecture: x86-64  19:39:59 up 12:23,  1 user,  load average: 0.09, 0.10, 0.08

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_IRQ_TIMER: Log("Received a timer event"); return schedule(c);
+    case EVENT_IRQ_TIMER: Log("Received a timer event"); return c; return schedule(c);
     case EVENT_YIELD: return schedule(c);
     case EVENT_SYSCALL: do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -5,17 +5,17 @@
 paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {
   paddr_t pg_table = paddr_read(cpu.cr3 + (vaddr >> 22) * sizeof(paddr_t), sizeof(paddr_t));
   if ((pg_table & 0x1) == 0) {
-    printf("cr3: 0x%08x, ptable: 0x%08x, vaddr: 0x%08x, type: %d, len: %d, pc: 0x%08x\n", cpu.cr3, pg_table, vaddr, type, len, cpu.eip);
+    Log("cr3: 0x%08x, ptable: 0x%08x, vaddr: 0x%08x, type: %d, len: %d, pc: 0x%08x\n", cpu.cr3, pg_table, vaddr, type, len, cpu.eip);
+    assert(false);
   }
-  assert(pg_table & 0x1);
+
   paddr_t pg_base = paddr_read((pg_table & ADDRMASK) + ((vaddr >> 12) & 0x3ff) * sizeof(paddr_t), sizeof(paddr_t));
   if ((pg_base & 0x1) == 0) {
-    printf("cr3: 0x%08x, ptable: 0x%08x, pbase: 0x%08x, vaddr: 0x%08x, type: %d, len: %d, pc: 0x%08x\n", cpu.cr3, pg_table, pg_base, vaddr, type, len, cpu.eip);
+    Log("cr3: 0x%08x, ptable: 0x%08x, pbase: 0x%08x, vaddr: 0x%08x, type: %d, len: %d, pc: 0x%08x\n", cpu.cr3, pg_table, pg_base, vaddr, type, len, cpu.eip);
+    assert(false);
   }
-  assert(pg_base & 0x1);
+  
   pg_base = pg_base & ADDRMASK;
-  // if (vaddr >= 0x40000000)
-    // printf("Translating 0x%08x to 0x%08x\n", vaddr, pg_base);
   if ((vaddr & FLAGMASK) + len - 1 > FLAGMASK)
     return pg_base | MEM_RET_CROSS_PAGE;
   return pg_base | MEM_RET_OK;
