09e0e931fbdb00b1bb7d1c6fcfa7b83c5f07d9d4
tracer-ics2020 <tracer@njuics.org>
2021-01-24 20:28:45 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 71d6739720d24cf492c5d295d069bf32     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-62-generic       Architecture: x86-64  20:28:45 up 13:12,  1 user,  load average: 0.30, 0.10, 0.02

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,7 +4,7 @@ static int times = 0;
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_IRQ_TIMER: Log("Received a timer event"); 
-      return ++times > 2 ? c : schedule(c);
+      return ++times > 3 ? c : schedule(c);
     case EVENT_YIELD: return schedule(c);
     case EVENT_SYSCALL: do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
