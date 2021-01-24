b7d34539ae7893320f21342ee88e6cd15db90064
tracer-ics2020 <tracer@njuics.org>
2021-01-24 20:01:54 +0800

    >  run 191220052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: a060d946020b4a8ca3135f0557ba8434            Boot ID: 71d6739720d24cf492c5d295d069bf32     Virtualization: vmware   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-62-generic       Architecture: x86-64  20:01:54 up 12:45,  1 user,  load average: 0.69, 0.19, 0.10

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -61,7 +61,7 @@ Context* schedule(Context *prev) {
   #ifdef DEBUG
     Log("[Schedule] Saved current context pointer to 0x%08x", prev);
   #endif
-  current = (current == &pcb[1] ? &pcb[1] : &pcb[1]);
+  current = (current == &pcb[1] ? &pcb[2] : &pcb[1]);
   #ifdef DEBUG
     Log("[Schedule] Switched to 0x%08x, entry 0x%08x", current, current->cp->eip);
   #endif
